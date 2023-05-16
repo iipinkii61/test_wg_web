f6eaf5b935fd62d1999cae50407cc73445c4c80c
iipinkii61 <bunyasitaa@gmail.com>
2023-05-16 18:04:49 +0700

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+// import './App.css'
+import styled from "styled-components";
+
+function App() {
+  const Button = styled.button`
+    border: 1px red solid;
+  `;
+
+  return <></>;
+}
+
+export default App;
